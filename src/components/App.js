ba488be97e9f6d40f3840602df3812ba153082dc
Ashutosh singh <ashutosh051995@gmail.com>
2020-11-11 14:10:01 +0530

    first commit shelling project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from "../logo.svg";
+import Button from "@material-ui/core/Button";
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+    </div>
+  );
+}
+
+export default App;
